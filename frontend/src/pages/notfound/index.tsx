567987eaba38cd26c3b8da4751c846ed46eed8c5
JGGrande <joaogabriel7303@gmail.com>
2024-11-29 13:10:36 -0300

    Adiciona estilos na página de não encontrado

--- frontend/src/pages/notfound/index.tsx
+++ frontend/src/pages/notfound/index.tsx
@@ -1,10 +1,15 @@
+import { EmptyState } from "@/components/ui/empty-state";
 import { Center } from "@chakra-ui/react";
+import { FiXOctagon } from "react-icons/fi";
 
 function NotFoundPage(){
 
   return (
-    <Center>
-      <h1>Página não encontrada.</h1>
+    <Center h="70vh">
+      <EmptyState
+        icon={<FiXOctagon color="red" />}
+        title="Página não encontrada"
+      />
     </Center>
   );
 }
